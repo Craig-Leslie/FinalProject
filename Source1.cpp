7f494535be02e4aee22fc2daa326f74f0bbb6f34
Craig-Leslie <craig.leslie1@outlook.com>
2026-02-19 14:01:08 +0000

    Add project files.

---
+++ Source1.cpp
@@ -0,0 +1,12 @@
+#include <opencv2/opencv.hpp>
+using namespace cv;
+int main(void) {
+	// Read image in GrayScale mode
+	Mat image = imread("boy.jpg", 0);
+	// Save grayscale image
+	imwrite("boyGray.jpg", image);
+	// To display the image
+	imshow("Grayscale Image", image);
+	waitKey(0);
+	return 0;
+}
